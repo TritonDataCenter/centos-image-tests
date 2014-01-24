34f8ba6de783fb5003f6ebe1b3fc6d1405b017b8
Christopher Horrell <christopher@joyent.com>
2014-01-24 23:00:22 +0000

    renamed product_file_spec.rb and added tests for MOTD

---
+++ spec/centos/product_and_branding_files_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+# Files that should be installed
+
+# product
+describe file('/etc/product') do
+	it { should be_file }
+  it { should be_linked_to '/lib/smartdc/product' }
+	it { should contain "Name: Joyent Instance" }
+	it { should contain "Image: #{attr[:name]} #{attr[:version]}" }
+	it { should contain "Documentation: #{attr[:doc_url]}" }
+end
+
+# MOTD
+describe file('/etc/motd') do
+  it { should be_file }
+  it { should contain "Instance (#{attr[:name]} #{attr[:version]}" }
+  it { should contain "#{attr[:doc_url]}" }
+	it { should be_linked_to '/etc/motd.joyent' }
+end
+
+describe file('/etc/issue') do
+  it { should be_file }
+  it { should be_linked_to '/etc/motd.joyent' }
+end
+
+describe file('/etc/issue.net') do
+  it { should be_file }
+  it { should be_linked_to '/etc/motd.joyent' }
+end
