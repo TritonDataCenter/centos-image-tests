0929faf992932775386e12214326d73a8223c109
Christopher Horrell <christopher@joyent.com>
2014-01-30 17:01:26 +0000

    MAke sure eth0 and eth1 are properly configured

--- spec/centos/network_spec.rb
+++ spec/centos/network_spec.rb
@@ -7,3 +7,18 @@ describe host('google.com') do
 	it { should be_reachable }
   it { should be_resolvable }
 end
+
+
+describe file('/etc/sysconfig/network-scripts/ifcfg-eth0') do
+	it { should be_file }
+  it { should contain 'DEVICE="eth0"' }
+	it { should contain 'ONBOOT="yes"' }
+	it { should contain 'BOOTPROTO="dhcp"' }
+end
+
+describe file('/etc/sysconfig/network-scripts/ifcfg-eth1') do
+  it { should be_file }
+  it { should contain 'DEVICE="eth1"' }
+  it { should contain 'ONBOOT="yes"' }
+  it { should contain 'BOOTPROTO="dhcp"' }
+end
