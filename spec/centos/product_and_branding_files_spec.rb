cece7e090ede2dfe09855a6056e7795557203ebc
Christopher Horrell <christopher@joyent.com>
2014-07-09 19:55:16 +0000

    removed check for things from older versions

--- spec/centos/product_and_branding_files_spec.rb
+++ spec/centos/product_and_branding_files_spec.rb
@@ -12,51 +12,20 @@ end
 
 ## MOTD
 # Since 2.6.1. motd is just a static file
-if attr[:version].delete('.').to_i <= 260
-  describe file('/etc/motd') do
-    it { should be_file }
-    it { should contain "Instance (#{attr[:name]} #{attr[:version]}" }
-    it { should contain "#{attr[:doc_url]}" }
-    it { should be_linked_to '/etc/motd.joyent' }
-  end
-  
-else
-  describe file('/etc/motd') do
-    it { should be_file }
-    it { should contain "Instance (#{attr[:name]} #{attr[:version]}" }
-    it { should contain "#{attr[:doc_url]}" }
-    it { should be_linked_to '/etc/motd' }
-  end
-  
+describe file('/etc/motd') do
+  it { should be_file }
+  it { should contain "Instance (#{attr[:name]} #{attr[:version]}" }
+  it { should contain "#{attr[:doc_url]}" }
 end
 
 # Since 2.6.1, link to the actual /etc/motd file
-if attr[:version].delete('.').to_i <= 260
-  describe file('/etc/issue') do
-    it { should be_file }
-    it { should be_linked_to '/etc/motd.joyent' }
-  end
-  
-else
-    
-  describe file('/etc/issue') do
-    it { should be_file }
-    it { should be_linked_to '/etc/motd' }
-  end
+describe file('/etc/issue') do
+  it { should be_file }
+  it { should be_linked_to '/etc/motd' }
 end
 
 # Since 2.6.1, link to the actual /etc/motd file
-if attr[:version].delete('.').to_i <= 260
-  describe file('/etc/issue.net') do
+describe file('/etc/issue.net') do
     it { should be_file }
-    it { should be_linked_to '/etc/motd.joyent' }
-  
-  end
-  
-else
-  describe file('/etc/issue.net') do
-      it { should be_file }
-      it { should be_linked_to '/etc/motd' }
-    end
-  
+    it { should be_linked_to '/etc/motd' }
 end
--- spec/centos/rootlogin_spec.rb
+++ spec/centos/rootlogin_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 # Make sure ssh login is via ssh key only. This is required since we are enabling
 # a password for root per IMAGE-459
-if attr[:version].delete('.').to_i >= 261
-  describe file('/etc/ssh/sshd_config') do
-    it { should be_file }
-    it { should contain "PasswordAuthentication no" }
-  end
+describe file('/etc/ssh/sshd_config') do
+  it { should be_file }
+  it { should contain "PasswordAuthentication no" }
 end
