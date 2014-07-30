4d9261a30efa29ada8005a659ac2e4f31c0e7845
Christopher Horrell <christopher@joyent.com>
2014-07-30 18:31:39 +0000

    Test to ensure 5 second grub timout. IMAGE-543

--- spec/centos/grub_spec.rb
+++ spec/centos/grub_spec.rb
@@ -4,6 +4,7 @@ describe file('/etc/grub.conf') do
 	it { should be_file }
 	it { should contain "tsc=reliable" }
   it { should contain "divider=10" }
+  it { should contain "GRUB_TIMEOUT=5" }
 end
 
 
