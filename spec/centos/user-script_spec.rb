2ddc9ddc19b037c9b043641192cc0ffcbf103a52
Christopher Horrell <christopher@joyent.com>
2014-08-18 15:27:40 +0000

    update user-script tests for sdc-vmtools

--- spec/centos/user-script_spec.rb
+++ spec/centos/user-script_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
-describe file('/var/run/smartdc/user-script') do
-	it { should be_file }
-  it { should be_owned_by 'root' }
+if attr[:version].to_i  < 20140818
+	describe file('/var/run/smartdc/user-script') do
+		it { should be_file }
+  	it { should be_owned_by 'root' }
+	end
+else
+  describe file('/var/tmp/user-script') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+  end
 end
 
-describe file('/var/lock/smartdc/user-script.lock') do
-  it { should be_file }
-  it { should be_owned_by 'root' }
+if attr[:version].to_i  < 20140818
+	describe file('/var/lock/smartdc/user-script.lock') do
+  	it { should be_file }
+  	it { should be_owned_by 'root' }
+	end
+else
+  describe file('/var/tmp/user-script.lock') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+  end
 end
 
 describe file('/var/tmp/test') do
