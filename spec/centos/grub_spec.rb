8751d5beb4de47c64b0f209d7c434d7e121c1061
Christopher Horrell <christopher@joyent.com>
2014-08-18 15:47:27 +0000

    CentOS 7 has newer grub

--- spec/centos/grub_spec.rb
+++ spec/centos/grub_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
-describe file('/etc/grub.conf') do
-	it { should be_file }
-	it { should contain "tsc=reliable" }
-  it { should contain "divider=10" }
-  it { should contain "GRUB_TIMEOUT=5" }
+# CentOS 7 hase newer Grub
+if attr[:name] != "CentOS 7.0-1406"
+	describe file('/etc/grub.conf') do
+		it { should be_file }
+		it { should contain "tsc=reliable" }
+  	it { should contain "divider=10" }
+  	it { should contain "GRUB_TIMEOUT=5" }
+	end
+else
+	describe file('/etc/default/grub') do
+  	it { should be_file }
+  	it { should contain "tsc=reliable" }
+  	it { should contain "divider=10" }
+  	it { should contain "GRUB_TIMEOUT=5" }
+	end
 end
-
-
