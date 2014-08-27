eaa51c1efa21e8c1a11f4ed139011a2de36c96b0
Christopher Horrell <christopher@joyent.com>
2014-08-27 21:23:36 +0000

    updated grub test for centos 6.5

--- spec/centos/grub_spec.rb
+++ spec/centos/grub_spec.rb
@@ -6,7 +6,7 @@ if property[:name] != "CentOS 7.0-1406"
 		it { should be_file }
 		it { should contain "tsc=reliable" }
   	it { should contain "divider=10" }
-  	it { should contain "GRUB_TIMEOUT=5" }
+  	it { should contain "timeout=5" }
 	end
 else
 	describe file('/etc/default/grub') do
