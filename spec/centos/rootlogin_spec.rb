cfcc7129fb092256ebd1ff34a195b4eeb0125744
Christopher Horrell <christopher@joyent.com>
2014-08-28 19:54:07 +0000

    Make sure password is set

--- spec/centos/rootlogin_spec.rb
+++ spec/centos/rootlogin_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+# Make sure a password is set IMAGE-459
+describe command("cat /etc/shadow | grep root | awk -F':' '{print $2;}'") do
+  it { should_not return_stdout "" }
+end
+
 # Make sure ssh login is via ssh key only. This is required since we are enabling
 # a password for root per IMAGE-459
 describe file('/etc/ssh/sshd_config') do
