e68e92a4d78e559f2960d4ec405756e9e07b51ec
Christopher Horrell <christopher@joyent.com>
2014-08-28 19:54:38 +0000

    Updated openssl test

--- spec/centos/corepackages_spec.rb
+++ spec/centos/corepackages_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 # Starting with 20140827
-if property[:version].to_i >= 20140827
-	describe package('1.0.1e-16.el6_5.15') do
+if property[:version].to_i >= 20140828
+	describe package('openssl-1.0.1e-16.el6_5.15') do
   	it { should be_installed }
 	end
 end
