9a77059d131e1fd05ea022d1aeface7685e80639
Christopher Horrell <christopher@joyent.com>
2014-11-28 17:36:30 +0000

    Ensure tty etc is setup for vmadm console. IMAGE-363

--- spec/centos/grub_spec.rb
+++ spec/centos/grub_spec.rb
@@ -7,6 +7,7 @@ if property[:name] != "CentOS 7.0-1406"
 		it { should contain "tsc=reliable" }
   	it { should contain "divider=10" }
   	it { should contain "timeout=5" }
+    it { should contain "console=ttyS0,115200n8 console=tty0" }
 	end
 else
 	describe file('/etc/default/grub') do
@@ -14,5 +15,6 @@ else
   	it { should contain "tsc=reliable" }
   	it { should contain "divider=10" }
   	it { should contain "GRUB_TIMEOUT=5" }
+    it { should contain "console=ttyS0" } 
 	end
 end
---
+++ spec/centos/ttyS0_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+# Ensure CentOS 6 images have an agetty service on ttyS0
+#
+# Note: With CentOS 7, systemd enables the service automatically
+# when you add console=ttyS0 on the kernel command line
+# http://0pointer.de/blog/projects/serial-console.html
+if property[:name] != "CentOS 7.0-1406"
+  describe file('/etc/init/ttyS0.conf') do
+    it { should be_file }
+    it { should contain "stop on runlevel [S016]" }
+    it { should contain "start on runlevel [23]" }
+    it { should contain "respawn" }
+    it { should contain "exec agetty -h -L -w /dev/ttyS0 115200 vt102" }
+  end
+end
+
