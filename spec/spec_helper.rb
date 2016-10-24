c978f530aef0cc501837ffb6d6467114304290a5
Christopher Horrell <christopher@joyent.com>
2016-10-24 15:40:20 -0400

    Add missing updates for helper script

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,14 +26,14 @@ options = Net::SSH::Config.for(host)
 options[:user] ||= Etc.getlogin
 
 set :host,        options[:host_name] || host
+
+# Get HostName and User value from Env Vars if available
+# This will override options in ~/.ssh/config or /etc/ssh_config
+options[:host_name] ||= ENV['TARGET_HOST_NAME']
+options[:user] ||= ENV['TARGET_USER_NAME']
+
 set :ssh_options, options
 
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C' 
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
+# Don't save hosts to ~/.ssh/known_hosts
+# When testing it's very common for the IPs to get reused and cause a mismatch
+set :user_known_hosts_file, '/dev/null'
