8716fcaa4fa955f7c9934c6820f04f2d7f84beba
Christopher Horrell <christopher@joyent.com>
2017-03-16 20:03:38 -0400

    Remove tests for old versions

--- spec/centos/packages_spec.rb
+++ spec/centos/packages_spec.rb
@@ -1,30 +1,5 @@
 require 'spec_helper'
 
-# Starting with 20140828
-if property[:version].to_i == 20140828
-	describe package('openssl-1.0.1e-16.el6_5.15') do
-  	it { should be_installed }
-	end
-end
-
-# Make sure we have the latest openssl to address HeartBleed bug
-# IMAGE-481
-if property[:version].to_i < 20140709
-	describe package('openssl-1.0.1e-16.el6_5.7') do
-  	it { should be_installed }
-	end
-end
-
-# Ensure latest availabe version of OpenSSL is installed
-# This will thow an error if openssl is newer but I want
-# to be explitily warned if teh version does not match
-# in future image versions.
-if property[:version].to_i == 20140710 
-  describe package('openssl-1.0.1e-16.el6_5.14') do
-    it { should be_installed }
-  end
-end
-
 describe package('acpid') do
   it { should be_installed }
 end
@@ -44,13 +19,6 @@ if property[:name].include? "CentOS 6"
 	end
 else
 	describe package('man-db') do
-  	it { should be_installed }
-	end
-end
-
-# We use install sdc-vmtools in newer versions rather than the rpm package
-if property[:version].to_i  < 20140818
-	describe package('me-centos') do
   	it { should be_installed }
 	end
 end
